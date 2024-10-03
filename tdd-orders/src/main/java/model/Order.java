0847d82861f3ea178d75400cf2321f6e4e7521cf
Camille Chauveau <camille.chauveau@outlook.fr>
2024-10-03 15:52:20 +0200

    Init test "placeOrderShouldCallRepositoryAndMapperOnce()" : do not compile

--- tdd-orders/src/main/java/model/Order.java
+++ tdd-orders/src/main/java/model/Order.java
@@ -2,7 +2,7 @@ package model;
 
 import enums.OrderStatus;
 
-import java.util.List;
+import java.util.Map;
 
-public record Order(String id, String product, int quantity, OrderStatus status, String customerId, List<String> productsIds) {
+public record Order(Long id, Long customerId, Map<Long, Integer> productsIdsAndQuantity, OrderStatus status) {
 }
--- tdd-orders/src/test/java/service/impl/OrderServiceImplTest.java
+++ tdd-orders/src/test/java/service/impl/OrderServiceImplTest.java
@@ -1,5 +1,57 @@
 package service.impl;
 
+import enums.OrderStatus;
+import model.Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import repository.OrderRepository;
+import service.OrderService;
+
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class OrderServiceImplTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+    @Spy
+    private OrderMapper orderMapper;
+    @InjectMocks
+    private OrderService orderService;
+
+    // Constantes
+    private final Long ID = 1L;
+    private final Long CUSTOMER_ID = 1L;
+    private final Map<Long, Integer> PRODUCTS_ID_AND_QUANTITY = Map.of();
+    private final OrderStatus STATUS = OrderStatus.PENDING;
+
+    @BeforeEach
+    public void setUp() {
+        this.orderMapper = new OrderMapperImpl();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void placeOrderShouldCallRepositoryAndMapperOnce() {
+        // Given
+        Order order = new Order(ID, CUSTOMER_ID, PRODUCTS_ID_AND_QUANTITY, STATUS);
+
+        // When
+        this.orderService.placeOrder(order);
+
+        // Then
+        verify(orderMapper, times(1)).toEntity(order);
+        verify(orderRepository, times(1)).save(any(OrderEntity.class));
+    }
 
 }
